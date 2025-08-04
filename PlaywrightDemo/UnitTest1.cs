13bd6b33fb92de87565da2083d3a41013825f666
massirr <124568542+massirr@users.noreply.github.com>
2025-08-04 20:43:16 +0200

    automation1

---
+++ PlaywrightDemo/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Playwright;
+
+namespace PlaywrightDemo;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public async Task Test1()
+    {
+        //Playwright
+        using var playwright = await Playwright.CreateAsync();
+        //Browser
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = false
+        });
+        //page
+        var page = await browser.NewPageAsync();
+        await page.GotoAsync($"http://192.168.129.83:8080/");
+        await page.ClickAsync(selector: "text=camp");
+        await page.ScreenshotAsync(new PageScreenshotOptions
+        {
+            Path = "CraftIt.jpg"
+        });
+    }
+}
